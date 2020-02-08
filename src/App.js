265ba35e0e33f3f0cb2e28c53ba14edf4445f480
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2020-02-08 01:26:10 -0300

    Iniciando a aplicação front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Inclusão de um componente
+import Login from './views/login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Inclusão do CSS
+import './custom.css'
+
+//Inclusão do CSS da biblioteca do Bootswatch
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Login></Login>
+      </div>
+    )
+  }
 }
 
 export default App;
