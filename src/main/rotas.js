a57939b0b84faed349f1ed9cdb8a683023e6e35d
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2020-02-09 00:06:43 -0300

    Implementação Lançamentos

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -3,8 +3,9 @@ import React from 'react'
 import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
 import Home from '../views/home'
+import ConsultaLancamentos from '../views/lancamentos/consulta-lancamentos'
 
-// {} significa uma desentruração
+// {} significa uma desentruturação
 import { Route, Switch, HashRouter } from 'react-router-dom'
 
 function Rotas(){
@@ -14,6 +15,7 @@ function Rotas(){
                 <Route path="/home" component={Home}></Route>
                 <Route path="/login" component={Login}></Route>
                 <Route path="/cadastro-usuario" component={CadastroUsuario}></Route>
+                <Route path="/consulta-lancamentos" component={ConsultaLancamentos}></Route>
             </Switch>
         </HashRouter>
     )
---
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import Card from '../../components/card'
+import FormGroup from '../../components/form-group'
+import SelectMenu from '../../components/selectMenu'
+import LancamentosTable from './lancamentosTable'
+import LancamentoService from '../../app/service/lancamentoService'
+import LocalStorageService from '../../app/service/localstorageService'
+
+class ConsultaLancamentos extends React.Component{
+    state = {
+        ano: '',
+        mes: '',
+        tipo: '',
+        lancamentos: []
+    }
+
+    constructor(){
+        super();
+        this.service = new LancamentoService()
+    }
+    buscar = () =>{
+        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+        const lancamentoFiltro = {
+            ano: this.state.ano,
+            mes: this.state.mes,
+            tipo: this.state.tipo,
+            usuario: usuarioLogado.id
+        }
+
+        this.service.consultar(lancamentoFiltro)
+        .then(resposta => {
+            this.setState({lancamentos: resposta.data})
+        }).catch( error => {
+            console.log(error)
+        })
+    }
+    render(){
+        const meses = [
+            {label: 'Selecione...', value: ''},
+            {label: 'Janeiro', value: 1},
+            {label: 'Fevereiro', value: 2},
+            {label: 'Março', value: 3},
+            {label: 'Abril', value: 4},
+            {label: 'Maio', value: 5},
+            {label: 'Junho', value: 6},
+            {label: 'Julho', value: 7},
+            {label: 'Agosto', value: 8},
+            {label: 'Setembro', value: 9},
+            {label: 'Outubro', value: 10},
+            {label: 'Novembro', value: 11},
+            {label: 'Dezembro', value: 12}
+        ]
+        const tipos = [
+            {label: 'Selecione...', value: ''},
+            {label: 'Despesa', value: 'DESPESA'},
+            {label: 'Receita', value: 'RECEITA'}
+        ]
+        return(
+            <Card title="Consulta Lançamentos">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="bs-component">
+                            <FormGroup htmlFor="inputAno" label="Ano: *">
+                                <input type="text"
+                                    className="form-control"
+                                    id="inputAno"
+                                    value={this.state.ano}
+                                    onChange={e => this.setState({ano: e.target.value})}
+                                    placeholder="Digite o Ano"></input>
+                            </FormGroup>
+                            <FormGroup htmlFor="inputMes" label="Mês: ">
+                                <SelectMenu id="inputMes" 
+                                    className="form-control" 
+                                    value={this.state.mes}
+                                    onChange={e => this.setState({mes: e.target.value})}
+                                    lista={meses}></SelectMenu>
+                            </FormGroup>
+                            <FormGroup htmlFor="inputTipo" label="Tipo Lançamentos: ">
+                                <SelectMenu id="inputTipo" 
+                                    className="form-control" 
+                                    value={this.state.tipo}
+                                    onChange={e => this.setState({tipo: e.target.value})}
+                                    lista={tipos}></SelectMenu>
+                            </FormGroup>
+                            <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
+                            <button type="button" className="btn btn-danger">Cadastrar</button>
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="bs-component">
+                            <LancamentosTable lancamentos={this.state.lancamentos}></LancamentosTable>
+                        </div>
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+}
+
+export default withRouter(ConsultaLancamentos)
